825ab40cfc8d4c46338334532c782a9bc833afeb
QueraltSosaMompel <queraltsosa@hotmail.com>
2018-10-30 09:15:43 +0000

    Initial Commit

---
+++ Read_Binary_Text/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Read_Binary_Text
+//
+//  Created by Queralt Sosa Mompel on 30/10/18.
+//  Copyright © 2018 Queralt Sosa Mompel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
